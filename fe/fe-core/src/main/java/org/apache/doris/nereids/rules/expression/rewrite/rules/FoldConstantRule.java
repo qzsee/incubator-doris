3357170043a0a4cc0b2502f2f9f709fea40514b5
shizhiqiang03 <shizhiqiang03@meituan.com>
2022-08-25 19:41:32 +0800

    111

--- fe/fe-core/src/main/java/org/apache/doris/nereids/rules/expression/rewrite/rules/FoldConstantRule.java
+++ fe/fe-core/src/main/java/org/apache/doris/nereids/rules/expression/rewrite/rules/FoldConstantRule.java
@@ -310,7 +310,7 @@ public class FoldConstantRule extends AbstractExpressionRewriteRule {
     }
 
     private Expression foldByBe(Expression root, ExpressionRewriteContext context) {
-        if (!root.isLiteral()) {
+        if (root.isConstant() && !root.isLiteral()) {
             Expr expr = ExpressionTranslator.INSTANCE.translate(root, null);
             IdGenerator<ExprId> idGenerator = ExprId.createGenerator();
             assignId(expr, idGenerator);
