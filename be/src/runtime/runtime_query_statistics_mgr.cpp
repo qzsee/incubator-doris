b254eb7354beb75f4bf82852f1ba3dd40b52db17
garenshi <garenshi@tencent.com>
2025-04-15 14:52:22 +0800

    [Improve](audit) add load info for active tasks and audit log

--- be/src/runtime/runtime_query_statistics_mgr.cpp
+++ be/src/runtime/runtime_query_statistics_mgr.cpp
@@ -508,10 +508,12 @@ void RuntimeQueryStatisticsMgr::get_active_be_tasks_block(vectorized::Block* blo
         SchemaScannerHelper::insert_int64_value(5, tqs.cpu_ms, block);
         SchemaScannerHelper::insert_int64_value(6, tqs.scan_rows, block);
         SchemaScannerHelper::insert_int64_value(7, tqs.scan_bytes, block);
-        SchemaScannerHelper::insert_int64_value(8, tqs.max_peak_memory_bytes, block);
-        SchemaScannerHelper::insert_int64_value(9, tqs.current_used_memory_bytes, block);
-        SchemaScannerHelper::insert_int64_value(10, tqs.shuffle_send_bytes, block);
-        SchemaScannerHelper::insert_int64_value(11, tqs.shuffle_send_rows, block);
+        SchemaScannerHelper::insert_int64_value(8, tqs.load_rows, block);
+        SchemaScannerHelper::insert_int64_value(9, tqs.load_bytes, block);
+        SchemaScannerHelper::insert_int64_value(10, tqs.max_peak_memory_bytes, block);
+        SchemaScannerHelper::insert_int64_value(11, tqs.current_used_memory_bytes, block);
+        SchemaScannerHelper::insert_int64_value(12, tqs.shuffle_send_bytes, block);
+        SchemaScannerHelper::insert_int64_value(13, tqs.shuffle_send_rows, block);
 
         std::stringstream ss;
         ss << resource_ctx->task_controller()->query_type();
