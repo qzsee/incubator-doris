291ae7fa984a33d9c35299431728b8c053144b4f
garenshi <garenshi@tencent.com>
2026-06-03 21:41:20 +0800

    [BUG] Respect backslash escapes inside quoted strings when deserializing array from text

--- be/src/core/data_type_serde/data_type_array_serde.cpp
+++ be/src/core/data_type_serde/data_type_array_serde.cpp
@@ -117,13 +117,17 @@ Status DataTypeArraySerDe::deserialize_one_cell_from_json(IColumn& column, Slice
     char quote_char = 0;
     for (int idx = 0; idx < slice_size; ++idx) {
         char c = slice[idx];
+        if (has_quote && c == '\\' && idx + 1 < slice_size) {
+            ++idx;
+            continue;
+        }
         if (c == '"' || c == '\'') {
             if (!has_quote) {
                 quote_char = c;
-                has_quote = !has_quote;
-            } else if (has_quote && quote_char == c) {
+                has_quote = true;
+            } else if (quote_char == c) {
                 quote_char = 0;
-                has_quote = !has_quote;
+                has_quote = false;
             }
         } else if (!has_quote && (c == '[' || c == '{')) {
             ++nested_level;
